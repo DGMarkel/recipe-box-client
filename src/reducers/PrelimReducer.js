2f46ac45200bffb95a87bd8389366304664c1a16
github username <github email address>
2019-03-13 19:17:49 -0400

    build out preliminary state structure for reducer

---
+++ src/reducers/PrelimReducer.js
@@ -0,0 +1,19 @@
+import Auth from '../modules/Auth'
+
+export default function prelimReducer(state={
+  user: {
+    username: '',
+    password: '',
+    email: '',
+    name: '',
+    auth: Auth.isUserAuthenticated(),
+    monsters: {
+      list: null,
+      listLoaded: false
+    },
+  },
+  monster: {
+    name: '',
+    description: ''
+  }
+})
