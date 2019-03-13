3cfbd35ac3cebd1d364c569c7ee5ef38844d51a6
github username <github email address>
2019-03-13 19:22:26 -0400

    add store, debug reducer

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom';
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose } from 'redux'
+import PrelimReducer from './reducers/PrelimReducer'
 
 import './index.css';
 import App from './App';
+
+const store = createStore(PrelimReducer,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+)
 
 ReactDOM.render(
   <Provider store={store}>
--- src/reducers/PrelimReducer.js
+++ src/reducers/PrelimReducer.js
@@ -16,4 +16,6 @@ export default function prelimReducer(state={
     name: '',
     description: ''
   }
-})
+}, action) {
+  
+}
