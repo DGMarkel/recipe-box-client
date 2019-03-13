1eec80cb8e96658f83a825e27e1106f10d46c7ad
github username <github email address>
2019-03-12 23:40:52 -0400

    add components, module

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Link, Redirect, Route } from 'react-router-dom'
+import Auth from './modules/Auth'
+import MonsterList from './components/MonsterList'
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      auth: Auth.isUserAuthenticated()
+    }
+  }
+  render() {
+    return (
+      <Router>
+      <div className="App">
+        <Route
+          exact path="/monsters"
+          render={()=><MonsterList />}
+        />
+      </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
