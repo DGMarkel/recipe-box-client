e943a8db3e755096c7e52d446830efd6c8cb01da
github username <github email address>
2019-03-13 00:21:45 -0400

    debug RegisterForm

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -13,43 +13,43 @@ export default class RegisterForm extends Component {
   }
 
   handleChange = e => {
-    const name = e.target.name.value;
+    const name = e.target.name;
     const value = e.target.value;
     this.setState({
-      [name]: value
+      [name]: value,
     });
   }
 
   render() {
     return (
       <div className="form">
-        <form onSubmit={ (event) => { this.props.RegisterFormSubmit(event, this.state) } }>
+        <form onSubmit={ (e) => { this.props.handleRegisterSubmit(e, this.state) } }>
           <input
             type="text"
             name="name"
             placeholder="name"
             value={this.state.name}
-            onChange={this.handleChange}
+            onChange={(e) => this.handleChange(e)}
           />
           <input
             type="text"
             name="username"
             placeholder="username"
-            value={this.state.username}
+            value={this.state.name}
             onChange={this.handleChange}
           />
           <input
             type="email"
             name="email"
             placeholder="email"
-            value={this.state.email}
+            value={this.state.name}
             onChange={this.handleChange}
           />
           <input
             type="password"
             name="password"
             placeholder="password"
-            value={this.state.password}
+            value={this.state.name}
             onChange={this.handleChange}
           />
           <input type="submit" value="Register!"/>
