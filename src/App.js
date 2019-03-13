d902521479343b01284b3c6f63eb7fc8ae59dd93
github username <github email address>
2019-03-13 01:35:17 -0400

    build out handleLoginSubmit

--- src/App.js
+++ src/App.js
@@ -38,6 +38,22 @@ class App extends Component {
     })
   }
 
+  handleLoginSubmit(e, data) {
+    e.preventDefault();
+    fetch('/login', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => res.json())
+    .then(res => {
+      console.log(res);
+    }).catch(err => {
+      console.log(err);
+    })
+  }
+
   render() {
     return (
       <Router>
