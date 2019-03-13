166f7dacfea01779d0975b09ed4f59722a9e09ec
github username <github email address>
2019-03-13 19:37:14 -0400

    build action

---
+++ src/actions/MonsterActions.js
@@ -0,0 +1,10 @@
+export function fetchMonsters() {
+    return (dispatch) => {
+      dispatch({ type: 'LOADING_MONSTERS' })
+      return fetch('/monsters')
+        .then(res => res.json())
+        .then(resJSON => {
+          dispatch({ type: 'LOADED_MONSTERS', payload: 'resJSON' })
+        }).catch(err => console.log(err))
+    }
+}
