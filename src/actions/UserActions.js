f9883ad7c37803277c2c5ca750fc8bcd47839742
github username <github email address>
2019-03-14 23:17:34 -0400

    begin to build action

--- src/actions/UserActions.js
+++ src/actions/UserActions.js
@@ -1,1 +1,17 @@
 import Auth from '../modules/Auth'
+
+export function fetchUserData(token) {
+  fetch('/profile', {
+    method: 'GET',
+    headers: {
+      token: Auth.getToken(),
+      'authorization':  `Token ${Auth.getToken()}`
+    }
+  }).then( res => res.json())
+  .then( res => {
+    this.setState({
+      myMonsters: res.monsters,
+      monstersLoaded: true,
+    })
+  }).catch( err => console.log(err))
+}
