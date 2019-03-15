f1bfec9c077a84d85eb060bc180a1714c74fd656
github username <github email address>
2019-03-14 23:22:05 -0400

    complete preliminary build user actions

--- src/actions/UserActions.js
+++ src/actions/UserActions.js
@@ -1,17 +1,17 @@
 import Auth from '../modules/Auth'
 
-export function fetchUserData(token) {
-  fetch('/profile', {
-    method: 'GET',
-    headers: {
-      token: Auth.getToken(),
-      'authorization':  `Token ${Auth.getToken()}`
-    }
-  }).then( res => res.json())
-  .then( res => {
-    this.setState({
-      myMonsters: res.monsters,
-      monstersLoaded: true,
-    })
-  }).catch( err => console.log(err))
+export function fetchUserData() {
+  return (dispatch) => {
+    dispatch({ type: 'LOADING_USER_DATA' })
+    fetch('/profile', {
+      method: 'GET',
+      headers: {
+        token: Auth.getToken(),
+        'authorization':  `Token ${Auth.getToken()}`
+      }
+    }).then( res => res.json())
+    .then( resJSON => {
+      dispatch({ type: 'LOAD_USER_DATA', payload: resJSON })
+    }).catch( err => console.log(err))
+  }
 }
