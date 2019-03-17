204dc7779759b29f8d120d513b776accbd2988a5
github username <github email address>
2019-03-16 21:57:29 -0400

    fix small issue with props

--- src/components/MyMonstersList.jsx
+++ src/components/MyMonstersList.jsx
@@ -3,10 +3,11 @@ import React, { Component } from 'react'
 export default class MyMonstersList extends Component {
 
   render() {
+
     return (
       <div>
-        { (this.props.monsters) ?
-          this.props.monsters.map( monster => <h1>{monster.name}</h1> )
+        { (this.props.myMonsters) ?
+          this.props.myMonsters.map( monster => <h1>{monster.name}</h1> )
           : <p>No monsters to display!</p>
         }
       </div>
