9d733a07fca5bf021529196f8d486cedc44c5490
github username <github email address>
2019-03-18 18:00:44 -0400

    add to form, and change handler

--- src/components/RecipeSearch.jsx
+++ src/components/RecipeSearch.jsx
@@ -9,12 +9,27 @@ export default class RecipeSearch extends Component {
     }
   }
 
+  handleOnChange = event => {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({
+      [name]: value
+    });
+  }
+
   render() {
+    console.log(this.state.name)
     return {
       <div className="search-form">
         <h1>Add a New Recipe</h1>
         <form>
-          <input type="text" placeholder="Ingredient" />
+          <input
+            type="text"
+            name="name"
+            value={this.state.name}
+            placeholder="Ingredient"
+            onChange={event => this.handleOnchange(event)}
+            />
           <input type="submit" value="Add Ingredient" />
         </form>
       </div>
