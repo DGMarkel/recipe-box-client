85ab6110157a5432e7fdab36ef7db2ebec7688fc
github username <github email address>
2019-03-20 19:06:28 -0400

    add display data

--- src/containers/RecipeContainer.jsx
+++ src/containers/RecipeContainer.jsx
@@ -6,7 +6,10 @@ export default class RecipeContainer extends Component {
   render() {
     return (
       <div className="recipe-card">
-
+        <h3>{this.props.recipe.title}</h3>
+        { this.props.recipe.ingredients.map(ingredient => {
+          return <IngredientContainer ingredient={ingredient} />
+        })}
       </div>
     )
   }
