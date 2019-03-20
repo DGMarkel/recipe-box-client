bc3d0503e23dcc8a6da479680d34c0f04b605689
github username <github email address>
2019-03-20 17:25:54 -0400

    remove constructor

--- src/components/RecipeSearch.jsx
+++ src/components/RecipeSearch.jsx
@@ -3,47 +3,12 @@ import RecipeContainer from '../containers/RecipeContainer'
 
 export default class RecipeSearch extends Component {
 
-  constructor() {
-    super()
-    this.state={
-      name: '',
-      recipeTitle: '',
-      recipeIngredients: [],
-      recipeLoaded: false
-    }
-  }
-
   handleOnChange = event => {
     const name = event.target.name;
     const value = event.target.value;
     this.setState({
       [name]: value
     });
-  }
-
-  handleOnSubmit = (event, data) => {
-    event.preventDefault()
-    fetch('https://trackapi.nutritionix.com/v2/natural/nutrients', {
-      method: 'POST',
-      body: JSON.stringify({
-        query: data
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-        'x-app-id': process.env.REACT_APP_NUTRITIONIX_APP_ID,
-        'x-app-key': process.env.REACT_APP_NUTRITIONIX_KEY,
-      }
-      }).then(res => res.json())
-      .then(res => {
-        console.log(res)
-        res.foods.map( ingredient => {
-          this.setState({
-              recipeIngredients: [
-                ...this.state.recipeIngredients.concat(ingredient)],
-              recipeLoaded: true
-          })
-        })
-      }).catch(err => console.log(err));
   }
 
   render() {
