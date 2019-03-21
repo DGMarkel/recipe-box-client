026225cc4eed83bbeb3e2ad4fda27581b7ace3a9
github username <github email address>
2019-03-20 21:37:13 -0400

    remove modules, remove AddMonsterForm, remove redux functions

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,35 +1,14 @@
 import React, { Component } from 'react'
-import AddMonsterForm from './AddMonsterForm'
-import MyMonstersList from './MyMonstersList'
 import RecipeSearch from './RecipeSearch'
-import * as actions from '../actions/MonsterActions'
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux'
 
-class DashBoard extends Component {
+export default class DashBoard extends Component {
 
   render() {
     return (
 
       <div>
-        <AddMonsterForm addMonster={this.props.addMonster} />
         <RecipeSearch />
-        <MyMonstersList myMonsters={this.props.monsters}/>
       </div>
     )
   }
 }
-
-const mapStateToProps = state => {
-  return {
-    monsters: state.user.monsters
-  }
-}
-
-const mapDispatchToProps = dispatch => {
-  return {
-    addMonster: bindActionCreators(actions.addMonster, dispatch)
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(DashBoard)
