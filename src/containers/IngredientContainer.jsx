90fc60a7a731a942165d2e443731f6a1813cf05b
github username <github email address>
2019-03-25 23:40:44 -0400

    add calorie coint to ingredients

--- src/containers/IngredientContainer.jsx
+++ src/containers/IngredientContainer.jsx
@@ -3,10 +3,12 @@ import React, { Component } from 'react'
 export default class IngredientContainer extends Component {
 
   render() {
+    console.log(this.props.ingredient)
     const ingredient = this.props.ingredient
     return (
       <div>
         <p>{ingredient.serving_qty} {ingredient.serving_unit} {ingredient.food_name}</p>
+        <p>Calories: {Math.floor(ingredient.calories)}</p>
       </div>
     )
   }
