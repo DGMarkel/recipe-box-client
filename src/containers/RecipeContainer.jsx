dc262b7305e10c393ff2af4ad0b4a7c4c26fbe92
github username <github email address>
2019-03-25 20:48:27 -0400

    add render function

--- src/containers/RecipeContainer.jsx
+++ src/containers/RecipeContainer.jsx
@@ -3,20 +3,24 @@ import IngredientContainer from './IngredientContainer'
 
 export default class RecipeContainer extends Component {
 
+  renderIngredients = () => {
+    return this.props.recipe.ingredients.map((ingredient, index) => {
+      return (
+        <div>
+          <IngredientContainer key={ingredient.food_name} ingredient={ingredient} />
+          <form onSubmit={e => this.props.deleteIngredient(e, this.props.recipe.id, index)}>
+            <input type="submit" value="Delete Ingredient" />
+          </form>
+        </div>
+      )
+    })
+  }
+
   render() {
     return (
       <div className="recipe-card">
         <h3>{this.props.recipe.title}</h3>
-        { this.props.recipe.ingredients.map((ingredient, index) => {
-          return (
-            <div>
-              <IngredientContainer key={ingredient.food_name} ingredient={ingredient} />
-              <form onSubmit={e => this.props.deleteIngredient(e, this.props.recipe.id, index)}>
-                <input type="submit" value="Delete Ingredient" />
-              </form>
-            </div>
-          )
-        })}
+        {this.renderIngredients()}
         <form onSubmit={event => {this.props.saveRecipe(event, this.props.recipe)}}>
           <input type="submit" value="Save Recipe" />
         </form>
