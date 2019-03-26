3efcc9f7d9fa784e5ffd1ba70e522b2e83d1c3c8
github username <github email address>
2019-03-25 21:24:53 -0400

    add new component

---
+++ src/components/AddRecipe.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import RecipeForm from './RecipeForm'
+import RecipeContainer from './containers/RecipeContainer'
+
+export default class NewRecipe {
+  render () {
+    <div>
+    </div>
+  }
+}
