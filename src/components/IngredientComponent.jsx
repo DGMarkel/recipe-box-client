ee0dc4184473c08a473e6c3bccac72e67d99c09b
github username <github email address>
2019-04-09 20:21:28 -0400

    move recipeTable to Ingredient component

--- src/components/IngredientComponent.jsx
+++ src/components/IngredientComponent.jsx
@@ -3,18 +3,25 @@ import React, { Component } from 'react'
 export default class IngredientContainer extends Component {
 
   render() {
-    console.log(this.props.ingredient)
-    const ingredient = this.props.ingredient
     return (
-      <div>
+      <table border="1">
         <tr>
-          <td>{ingredient.food_name}, {ingredient.serving_qty} {ingredient.serving_unit}</td>
-          <td>{Math.floor(ingredient.calories)}</td>
-          <td>{ingredient.total_carbohydrates}</td>
-          <td>{ingredient.protein}</td>
-          <td>{ingredient.sugars}</td>
+          <th>Ingredient</th>
+          <th>Calories(kCal)</th>
+          <th>Carbohydrates(g)</th>
+          <th>Protein(g)</th>
+          <th>Sugars(g)</th>
         </tr>
-      </div>
-    )
-  }
+        {this.props.recipe.ingredients.map((ingredient, index) => {
+          return <tr key={index}>
+            <td>{ingredient.food_name}, {ingredient.serving_qty} {ingredient.serving_unit}</td>
+            <td>{Math.floor(ingredient.calories)}</td>
+            <td>{ingredient.total_carbohydrates}</td>
+            <td>{ingredient.protein}</td>
+            <td>{ingredient.sugars}</td>
+          </tr>
+        })}
+        </table>
+      )
+    }
 }
--- src/containers/RecipesContainer.jsx
+++ src/containers/RecipesContainer.jsx
@@ -1,37 +1,16 @@
 import React, { Component } from 'react'
 import './RecipesContainer.css'
+import IngredientComponent from '../components/IngredientComponent'
 
 export default class RecipesContainer extends Component {
 
-  recipeTable = () => {
-    return <table border="1">
-      <tr>
-        <th>Ingredient</th>
-        <th>Calories(kCal)</th>
-        <th>Carbohydrates(g)</th>
-        <th>Protein(g)</th>
-        <th>Sugars(g)</th>
-      </tr>
-      {this.props.recipe.ingredients.map((ingredient, index) => {
-        return <tr key={index}>
-          <td>{ingredient.food_name}, {ingredient.serving_qty} {ingredient.serving_unit}</td>
-          <td>{Math.floor(ingredient.calories)}</td>
-          <td>{ingredient.total_carbohydrates}</td>
-          <td>{ingredient.protein}</td>
-          <td>{ingredient.sugars}</td>
-        </tr>
-      })}
-    </table>
-  }
-
-
-
   render() {
+    console.log(this.props.recipe)
     return (
       <div className="recipe-card">
         <h1>{this.props.recipe.title}</h1>
         <p>{this.props.recipe.description}</p>
-        {this.recipeTable()}
+        <IngredientComponent recipe={this.props.recipe} />
       </div>
     )
   }
