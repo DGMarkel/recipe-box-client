1b35be7e07b582393e4d190498928ba98ea1dceb
github username <github email address>
2019-04-11 00:28:30 -0400

    update navbar

--- src/App.js
+++ src/App.js
@@ -10,8 +10,6 @@ export default class App extends Component {
          <h1>Recipe Box</h1>
        </div>
        <div className="nav">
-         <a href="/login">Login</a>
-         <a href="/register">Register</a>
          <a href="/dash">Dashboard</a>
          <a href="/recipes">Recipes</a>
          <a href="/logout" onClick={this.props.handleLogout}>Logout</a>
