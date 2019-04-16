cd307701f7777ddbb3b9007c1933c98269dd0469
github username <github email address>
2019-04-16 18:22:05 -0400

    add local state

--- src/containers/EditRecipe.jsx
+++ src/containers/EditRecipe.jsx
@@ -4,6 +4,17 @@ import { withRouter } from 'react-router-dom'
 import RecipeCard from '../components/RecipeCard'
 
 class EditRecipe extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      id: props.location.state.recipe.id,
+      title: props.location.state.recipe.title,
+      image_url: props.location.state.recipe.image_url,
+      description: props.location.state.recipe.description,
+      ingredients: props.location.state.recipe.ingredients
+    }
+  }
+
   render() {
     return (
       <></>
