39d52f036292767e77777219093e642dda1db0c1
github username <github email address>
2019-04-16 18:03:42 -0400

    remove form, will try a new approach to editing recipes

--- src/containers/EditRecipe.jsx
+++ src/containers/EditRecipe.jsx
@@ -1,21 +1,8 @@
 import React, { Component } from 'react'
-import * as actions from '../actions/RecipeActions'
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux'
-import { withRouter } from 'react-router-dom'
+
+import RecipeCard from '../components/RecipeCard'
 
 class EditRecipe extends Component {
-  constructor(props) {
-    super(props)
-    this.state={
-      id: props.location.state.recipe.id,
-      title: props.location.state.recipe.title,
-      image_url: props.location.state.recipe.image_url,
-      description: props.location.state.recipe.description,
-      ingredients: props.location.state.recipe.ingredients
-    }
-  }
-
   render() {
     return (
       <></>
