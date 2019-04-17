425f4403d2d16e4ae0d87dff38c06d219f5c9ab8
github username <github email address>
2019-04-16 22:40:22 -0400

    update state, add function, add submit button

--- src/containers/EditRecipe.jsx
+++ src/containers/EditRecipe.jsx
@@ -10,15 +10,16 @@ class EditRecipe extends Component {
     super(props)
     this.state = {
       id: props.location.state.recipe.id,
-      title: '',
-      image_url: '',
-      description: '',
-      ingredients: ''
+      title: props.location.state.recipe.title,
+      image_url: props.location.state.recipe.image_url,
+      description: props.location.state.recipe.description,
+      ingredients: props.location.state.recipe.ingredients
     }
   }
 
   editRecipe = (e, data) => {
     e.preventDefault();
+    debugger
     fetch('/edit', {
       method: 'PATCH',
       body: JSON.stringify({
@@ -36,7 +37,7 @@ class EditRecipe extends Component {
       console.log(err);
     })
   }
-  
+
 
   handleOnChange = event => {
     const ingredients = event.target.name;
@@ -46,15 +47,38 @@ class EditRecipe extends Component {
     });
   }
 
+  handleIngredientUpdate = (e, index) => {
+    const data_type = e.target.name;
+    const portion_data = e.target.value;
+    let new_ingredients = [...this.state.ingredients];
+    let ingredient = {...new_ingredients[index]};
+    ingredient[data_type] = portion_data;
+    new_ingredients[index] = ingredient
+    this.setState({
+      ingredients: new_ingredients
+    });
+  }
+
   renderIngredientsInForm = () => {
-    return this.state.ingredients.map(ingredient => {
-      console.log(ingredient)
+    return this.state.ingredients.map((ingredient, index) => {
       return (
         <div className="ingredient">
           <h3>{ingredient.food_name}</h3>
           <p>Calories: {ingredient.calories} Total Fat: {ingredient.total_fat} Protein: {ingredient.protein} Carbs: {ingredient.total_carbohydrate}</p>
-          Quantity: <input type="text" value={ingredient.serving_qty} onChange={e=>this.handleOnChange(e)} />
-          Serving Unit: <input type="text" value={ingredient.serving_unit} /><br />
+          Quantity:
+            <input
+              type="text"
+              name="serving_qty"
+              value={ingredient.serving_qty}
+              onChange={e=>this.handleIngredientUpdate(e, index)}
+            />
+          Serving Unit:
+            <input
+              type="text"
+              name="serving_unit"
+              value={ingredient.serving_unit}
+              onChange={e=>this.handleIngredientUpdate(e, index)}
+            /><br />
           <input type="submit" value={`Delete ${ingredient.food_name}`}/>
           <hr />
         </div>
@@ -88,6 +112,7 @@ class EditRecipe extends Component {
             onChange={event => this.handleOnChange(event)}
           /><br />
           { this.renderIngredientsInForm() }
+          <input type="submit" value="Update Recipe" />
         </form>
       </>
     )
