2673fb354045460527947af0ebf0914ecec6dba5
github username <github email address>
2019-04-19 11:56:34 -0400

    remove imported component, replace with jsx, rename class

--- src/containers/NewRecipeContainer.jsx
+++ src/containers/NewRecipeContainer.jsx
@@ -3,15 +3,18 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux'
 
 import IngredientsTable from '../components/IngredientsTable'
-import NewRecipeCard from '../components/NewRecipeCard'
 import * as actions from '../actions/RecipeActions'
 
 class RecipeContainer extends Component {
 
   render() {
     return (
-      <div className="recipe-card">
-        <NewRecipeCard recipe={this.props.recipe} />
+      <div className="new-recipe">
+        <div className="recipe-header">
+          <h3 className="title">{this.props.recipe.title}</h3>
+        </div>
+        <img src={this.props.recipe.image_url} />
+        <p>{this.props.recipe.description}</p>
         <IngredientsTable recipe={this.props.recipe} />
         <form onSubmit={event => {this.props.saveRecipe(event, this.props.recipe)}}>
           <input type="submit" value="Save Recipe" />
