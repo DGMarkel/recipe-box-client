918404fbfc00c817c9c6ebfd7957972368cc1f9e
github username <github email address>
2019-04-24 22:31:17 -0400

    move fetchIngredients from actions to container, disconnect container from global state, update callbacks accordingly

--- src/containers/NewRecipeForm.jsx
+++ src/containers/NewRecipeForm.jsx
@@ -40,12 +40,53 @@ class NewRecipeForm extends Component {
     })
   }
 
+  fetchIngredients = event => {
+    event.preventDefault()
+      fetch('https://trackapi.nutritionix.com/v2/natural/nutrients', {
+      method: 'POST',
+      body: JSON.stringify({
+        query: this.state.ingredients_to_fetch
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+        'x-app-id': process.env.REACT_APP_NUTRITIONIX_APP_ID,
+        'x-app-key': process.env.REACT_APP_NUTRITIONIX_KEY,
+      }
+      }).then(res => res.json())
+      .then(res => {
+        const ingredientList = res.foods.map( ingredient => {
+          let ingredientList = {}
+          ingredientList["food_name"] = ingredient.food_name;
+          ingredientList["serving_qty"] = ingredient.serving_qty;
+          ingredientList["serving_unit"] = ingredient.serving_unit;
+          ingredientList["calories"] = ingredient.nf_calories;
+          ingredientList["total_fat"] = ingredient.nf_total_fat;
+          ingredientList["saturated_fat"] = ingredient.nf_saturated_fat;
+          ingredientList["cholesterol"] = ingredient.nf_cholesterol;
+          ingredientList["sodium"] = ingredient.nf_sodium;
+          ingredientList["total_carbohydrate"] = ingredient.nf_total_carbohydrate;
+          ingredientList["dietary_fiber"] = ingredient.nf_dietary_fiber;
+          ingredientList["sugars"] = ingredient.nf_sugars;
+          ingredientList["protein"] = ingredient.nf_protein;
+          ingredientList["potassium"] = ingredient.nf_potassium;
+          return ingredientList
+        })
+        this.setState({
+          ...this.state,
+          recipe: {
+            ...this.state.recipe,
+            ingredients: ingredientList
+          }
+        })
+      }).catch(err => console.log(err));
+  }
+
   render() {
     return (
       <div>
         <div className="recipe-form">
           <h1>Create a New Recipe</h1>
-          <form onSubmit={(e) => this.props.fetchIngredients(e, this.state.ingredients_to_fetch)}>
+          <form onSubmit={e => this.fetchIngredients(e)}>
             <textarea
               cols="60"
               name="title"
@@ -79,7 +120,7 @@ class NewRecipeForm extends Component {
           </form>
         </div>
         <div className="recipe-container">
-
+          <NewRecipeContainer recipe={this.state.recipe} />
         </div>
       </div>
     )
@@ -98,4 +139,4 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(NewRecipeForm)
+export default NewRecipeForm
