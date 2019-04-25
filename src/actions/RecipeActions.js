c55e5a357d8a7a617e3c7469bb535f3b979ce280
github username <github email address>
2019-04-24 22:33:59 -0400

    remove unused action

--- src/actions/RecipeActions.js
+++ src/actions/RecipeActions.js
@@ -49,43 +49,6 @@ export function updateIngredient(event, recipeID, ingredient) {
   }
 }
 
-
-export function fetchIngredients(event, title, image_url, id, ingredients, description) {
-  event.preventDefault()
-  return (dispatch) => {
-    fetch('https://trackapi.nutritionix.com/v2/natural/nutrients', {
-    method: 'POST',
-    body: JSON.stringify({
-      query: ingredients
-    }),
-    headers: {
-      'Content-Type': 'application/json',
-      'x-app-id': process.env.REACT_APP_NUTRITIONIX_APP_ID,
-      'x-app-key': process.env.REACT_APP_NUTRITIONIX_KEY,
-    }
-    }).then(res => res.json())
-    .then(res => {
-      const ingredientList = res.foods.map( ingredient => {
-        let ingredientList = {}
-        ingredientList["food_name"] = ingredient.food_name;
-        ingredientList["serving_qty"] = ingredient.serving_qty;
-        ingredientList["serving_unit"] = ingredient.serving_unit;
-        ingredientList["calories"] = ingredient.nf_calories;
-        ingredientList["total_fat"] = ingredient.nf_total_fat;
-        ingredientList["saturated_fat"] = ingredient.nf_saturated_fat;
-        ingredientList["cholesterol"] = ingredient.nf_cholesterol;
-        ingredientList["sodium"] = ingredient.nf_sodium;
-        ingredientList["total_carbohydrate"] = ingredient.nf_total_carbohydrate;
-        ingredientList["dietary_fiber"] = ingredient.nf_dietary_fiber;
-        ingredientList["sugars"] = ingredient.nf_sugars;
-        ingredientList["protein"] = ingredient.nf_protein;
-        ingredientList["potassium"] = ingredient.nf_potassium;
-        return ingredientList
-      })
-    }).catch(err => console.log(err));
-  }
-}
-
 export function deleteIngredient(e, recipeId, ingredient) {
   e.preventDefault();
   return (dispatch) => {
