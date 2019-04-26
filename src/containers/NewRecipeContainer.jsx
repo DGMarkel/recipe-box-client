2a7a23d843de61feac71c28a26df8885169418b4
github username <github email address>
2019-04-26 19:07:40 -0400

    add alt to img tag

--- src/containers/NewRecipeContainer.jsx
+++ src/containers/NewRecipeContainer.jsx
@@ -13,7 +13,7 @@ class RecipeContainer extends Component {
         <div className="recipe-header">
           <h3 className="title">{this.props.recipe.title}</h3>
         </div>
-        <img src={this.props.recipe.image_url} />
+        <img src={this.props.recipe.image_url}  alt={this.props.recipe.title}/>
         <p>{this.props.recipe.description}</p>
         { (this.props.recipe.ingredients)
           ? <IngredientsTable recipe={this.props.recipe} />
