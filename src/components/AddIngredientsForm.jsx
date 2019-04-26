f61687cae9e7b76a389dce3c1b7b7835fae28e31
github username <github email address>
2019-04-26 16:04:03 -0400

    build new component

---
+++ src/components/AddIngredientsForm.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class AddIngredientsForm extends Component {
+  render() {
+    return (
+      <form onSubmit={e => this.props.fetchAndPostIngredients(e, this.state) }>
+        <textarea
+          rows="10"
+          cols="60"
+          name="rawIngredients"
+          value={this.state.rawIngredients}
+          placeholder="Ingredient"
+          onChange={e => this.handleOnChange(e)}
+          /><br />
+        <input type="submit" value="Save Ingredients"/>
+      </form>
+    )
+  }
+}
