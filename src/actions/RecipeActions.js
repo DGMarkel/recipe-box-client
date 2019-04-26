35d0e8718d5e09095db03fefd179ffd5ac004b49
github username <github email address>
2019-04-26 15:38:02 -0400

    move fetchIngredients and postIngredients to RecipeActions

--- src/actions/RecipeActions.js
+++ src/actions/RecipeActions.js
@@ -1,5 +1,61 @@
 import Auth from '../modules/Auth'
 
+export function fetchIngredients(event, rawIngredients) {
+  event.preventDefault()
+    fetch('https://trackapi.nutritionix.com/v2/natural/nutrients', {
+    method: 'POST',
+    body: JSON.stringify({
+      query: rawIngredients
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+      'x-app-id': process.env.REACT_APP_NUTRITIONIX_APP_ID,
+      'x-app-key': process.env.REACT_APP_NUTRITIONIX_KEY,
+    }
+    }).then(res => res.json())
+    .then(res => {
+      const ingredientList = res.foods.map( ingredient => {
+        let ingredientList = {}
+        ingredientList["food_name"] = ingredient.food_name;
+        ingredientList["serving_qty"] = ingredient.serving_qty;
+        ingredientList["serving_unit"] = ingredient.serving_unit;
+        ingredientList["calories"] = ingredient.nf_calories;
+        ingredientList["total_fat"] = ingredient.nf_total_fat;
+        ingredientList["saturated_fat"] = ingredient.nf_saturated_fat;
+        ingredientList["cholesterol"] = ingredient.nf_cholesterol;
+        ingredientList["sodium"] = ingredient.nf_sodium;
+        ingredientList["total_carbohydrate"] = ingredient.nf_total_carbohydrate;
+        ingredientList["dietary_fiber"] = ingredient.nf_dietary_fiber;
+        ingredientList["sugars"] = ingredient.nf_sugars;
+        ingredientList["protein"] = ingredient.nf_protein;
+        ingredientList["potassium"] = ingredient.nf_potassium;
+        return ingredientList
+      })
+      postIngredients(ingredientList)
+    }).catch(err => console.log(err));
+}
+
+function postIngredients(ingredients) {
+  fetch('/edit-recipe', {
+    method: 'PATCH',
+    body: JSON.stringify({
+      recipe: {
+        id: this.state.recipe.id,
+        title: this.state.recipe.title,
+        description: this.state.recipe.description,
+        image_url: this.state.recipe.image_url,
+        ingredients: ingredients
+      }
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+      token: Auth.getToken(),
+      'authorization':  `Token ${Auth.getToken()}`
+    }
+  })
+}
+
+
 export function updateRecipeDetails(event, recipe) {
   event.preventDefault();
 
