b2169c7780e92b323b91248fedd241559d38bf11
github username <github email address>
2019-04-27 15:49:55 -0400

    update routes

--- src/routes.js
+++ src/routes.js
@@ -14,13 +14,13 @@ import FullRecipeCard from './components/FullRecipeCard'
 export default (
     <Switch id='routes'>
       <Route exact path='/' render={ () => <Recipes/> }/>
-      <Route path='/signup' component={ () => Auth.isUserAuthenticated() ? <Redirect to="/"/> : <SignUp/> }/>
-      <Route path='/login' component={ () => Auth.isUserAuthenticated() ? <Redirect to="/dash"/> : <Login/> }/>
+      <Route exact path='/signup' component={ () => Auth.isUserAuthenticated() ? <Redirect to="/"/> : <SignUp/> }/>
+      <Route exact path='/login' component={ () => Auth.isUserAuthenticated() ? <Redirect to="/dash"/> : <Login/> }/>
 
-      <Route path='/dash' component={ () =>  Auth.isUserAuthenticated() ? <Dashboard/> : <Redirect to="/login"/> }/>
+      <Route exact path='/dash' component={ () =>  Auth.isUserAuthenticated() ? <Dashboard/> : <Redirect to="/login"/> }/>
 
       <Route exact path='/recipes' component={ () =>  <Recipes/> }/>
-      <Route path='/my-recipes' component={ () =>  Auth.isUserAuthenticated() ? <UserRecipes/> : <Redirect to="/login"/> }/>
+      <Route exact path='/my-recipes' component={ () =>  Auth.isUserAuthenticated() ? <UserRecipes/> : <Redirect to="/login"/> }/>
       <Route path='/recipes/new' component={ () =>  Auth.isUserAuthenticated() ? <NewRecipe/> : <Redirect to="/login"/> }/>
       <Route exact path='/recipes/:recipeName' component={ () => <FullRecipeCard/> }/>
       // will need to update edit route to only show to owner
