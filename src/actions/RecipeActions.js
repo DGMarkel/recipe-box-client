49aab9d8a19e23b85753ddaf1a0bee23ee57c6e8
github username <github email address>
2019-04-27 12:29:08 -0400

    move updateIngredient from actions to editRecipe

--- src/actions/RecipeActions.js
+++ src/actions/RecipeActions.js
@@ -92,61 +92,6 @@ export function updateRecipeDetails(event, recipe) {
   }
 }
 
-export function updateIngredient(event, recipeID, ingredient, ingredientIndex) {
-
-  event.preventDefault();
-
-  if (ingredient.food_name === ingredient.serving_unit) delete ingredient.serving_unit;
-  const ingredientString = `${ingredient.serving_qty} ${ingredient.serving_unit || ''} ${ingredient.food_name}`
-
-  return(dispatch) => {
-    fetch('https://trackapi.nutritionix.com/v2/natural/nutrients', {
-    method: 'POST',
-    body: JSON.stringify({
-      query: ingredientString
-    }),
-    headers: {
-      'Content-Type': 'application/json',
-      'x-app-id': process.env.REACT_APP_NUTRITIONIX_APP_ID,
-      'x-app-key': process.env.REACT_APP_NUTRITIONIX_KEY,
-    }
-  }).then(res => res.json())
-  .then(res => {
-      console.log(res)
-      let updatedIngredient = {}
-      updatedIngredient["food_name"] = res.foods[0].food_name;
-      updatedIngredient["serving_qty"] = res.foods[0].serving_qty;
-      updatedIngredient["serving_unit"] = res.foods[0].serving_unit;
-      updatedIngredient["calories"] = res.foods[0].nf_calories;
-      updatedIngredient["total_fat"] = res.foods[0].nf_total_fat;
-      updatedIngredient["saturated_fat"] = res.foods[0].nf_saturated_fat;
-      updatedIngredient["cholesterol"] = res.foods[0].nf_cholesterol;
-      updatedIngredient["sodium"] = res.foods[0].nf_sodium;
-      updatedIngredient["total_carbohydrate"] = res.foods[0].nf_total_carbohydrate;
-      updatedIngredient["dietary_fiber"] = res.foods[0].nf_dietary_fiber;
-      updatedIngredient["sugars"] = res.foods[0].nf_sugars;
-      updatedIngredient["protein"] = res.foods[0].nf_protein;
-      updatedIngredient["potassium"] = res.foods[0].nf_potassium;
-      dispatch({ type:'UPDATE_INGREDIENT', payload: { updatedIngredient: updatedIngredient, ingredientIndex: ingredientIndex } })
-
-      fetch('/edit-ingredient', {
-        method: 'PATCH',
-        body: JSON.stringify({
-            ingredient: {
-              ingredient_data: updatedIngredient,
-              recipeID: recipeID
-            }
-          }),
-        headers: {
-          'Content-Type': 'application/json',
-          token: Auth.getToken(),
-          'authorization':  `Token ${Auth.getToken()}`
-        }
-      })
-    }).then(dispatch({ type: 'TOGGLE_INGREDIENT_UPDATED'})).catch(err => console.log(err));
-  }
-}
-
 export function clearUpdatedIngredient() {
   return (dispatch) => {
     dispatch({ type: 'CLEAR_UPDATED_INGREDIENT'})
