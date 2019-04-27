a29ac7ed3f5c13c240af5dd7205b84ac25421996
github username <github email address>
2019-04-27 17:00:31 -0400

    add bootstrap to package.json

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose } from 'redux'
 import { BrowserRouter } from 'react-router-dom'
 import PrelimReducer from './reducers/PrelimReducer'
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 
