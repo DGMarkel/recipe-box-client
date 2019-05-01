11248f95d6dc2a6a809a099335d13136a2ee0559
github username <github email address>
2019-04-30 23:48:36 -0400

    remove conditional

--- src/containers/Recipes.jsx
+++ src/containers/Recipes.jsx
@@ -19,7 +19,7 @@ class Recipes extends Component {
   }
 
   componentDidUpdate() {
-    if (this.state.search_term === '' && !this.props.user) {
+    if (this.state.search_term === '') {
       this.allRecipes()
     }
   }
