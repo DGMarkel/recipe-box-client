97947f6e679bf46c593475dca85bbde80c0af00c
github username <github email address>
2019-05-01 10:25:39 -0400

    remove all methods, Auth import

--- src/components/FullRecipeCard.jsx
+++ src/components/FullRecipeCard.jsx
@@ -1,32 +1,8 @@
 import React, { Component } from 'react'
 import IngredientsTable from '../components/IngredientsTable'
 import { withRouter } from 'react-router-dom'
-import Auth from '../modules/Auth'
 
 class FullRecipeCard extends Component {
-
-  componentDidMount() {
-    fetch(`/recipes/${this.props.location.state.recipe.id}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        token: Auth.getToken(),
-        'authorization':  `Token ${Auth.getToken()}`
-      }
-    }).then(res => res.json())
-    .then(res => {
-      this.setState({
-        recipe: {
-          id: res.id,
-          title: res.title,
-          description: res.description,
-          image_url: res.image_url,
-          ingredients: res.ingredients,
-          recipe_totals: res.recipe_totals
-        }
-      })
-    })
-  }
 
   recipe = this.props.location.state.recipe
 
