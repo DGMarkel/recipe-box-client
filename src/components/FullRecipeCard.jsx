f9396c34bc05676eb35493534ad43e50f27783a9
github username <github email address>
2019-05-01 12:05:14 -0400

    format full recipe card

--- src/components/FullRecipeCard.jsx
+++ src/components/FullRecipeCard.jsx
@@ -7,11 +7,13 @@ class FullRecipeCard extends Component {
   recipe = this.props.location.state.recipe
 
   render() {
+    console.log(this.recipe)
     return (
       <>
         <div className="full-recipe-card">
           <h1>{this.recipe.title}</h1>
-          <em>{this.recipe.description}</em>
+          <p>{this.recipe.description}</p>
+          <h4>Calories: {this.recipe.recipe_totals.calories}</h4>
           <img src={this.recipe.image_url} alt={this.recipe.title} />
         </div>
         <div className="ingredients-table">
