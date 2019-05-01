9132d391eb42bb566e1a5ae89a6fb083aa573588
github username <github email address>
2019-05-01 14:51:35 -0400

    add input for servings

--- src/components/NewRecipeForm.jsx
+++ src/components/NewRecipeForm.jsx
@@ -36,6 +36,13 @@ class NewRecipeForm extends Component {
               placeholder="Ingredient"
               onChange={event => this.props.handleOnChangeForIngredients(event)}
               /><br />
+              <input
+                type="number"
+                value={this.props.recipe.servings}
+                name="servings"
+                placeholder="Servings"
+                onChange={event => this.props.handleOnChange(event)}
+                /><br />
             <input type="submit" value="Add Ingredients" />
           </form>
       </div>
