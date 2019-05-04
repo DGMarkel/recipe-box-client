5dd4627bf379eab17dc8c06b240f41f4e90820d8
github username <github email address>
2019-05-04 18:52:20 -0400

    remove div, add text, style welcome div

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -5,8 +5,7 @@ export default class Welcome extends Component {
   render() {
     return (
       <div className="welcome">
-        <div className="welcome-bg">
-        </div>
+        <h1>Recipe box is a nutritional tracker for home cooks who love to experiment in the kitchen</h1>
       </div>
     )
   }
