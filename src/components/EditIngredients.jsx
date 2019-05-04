f1441e5228978d34779522c7ea206273dfcff1d8
github username <github email address>
2019-05-04 15:05:11 -0400

    move buttons

--- src/components/EditIngredients.jsx
+++ src/components/EditIngredients.jsx
@@ -19,36 +19,34 @@ export default class EditIngredients extends Component {
         {
           this.props.ingredients.map((ingredient, index) => {
             return (
-              <tr>
-                <td>{ingredient.food_name}</td>
-                <td>
-                  <input
-                    type="text"
-                    name="serving_qty"
-                    value={ingredient.serving_qty}
-                    onChange={e=>this.props.handleOnChange(e, index)}
-                  />
-                </td>
-                <td>
-                  <input
-                    type="text"
-                    name="serving_unit"
-                    value={ingredient.serving_unit}
-                    onChange={e=>this.props.handleOnChange(e, index)}
-                  />
-                </td>
-                <td>{ingredient.calories}</td>
-                <td>{ingredient.total_carbohydrate}</td>
-                <td>{ingredient.protein}</td>
-                <td>{ingredient.sugars}</td>
-                <td>{ingredient.total_fat}</td>
-                <td>
-                  <input type="submit" value={`Update ${ingredient.food_name}`} onClick={e => this.props.updateIngredient(e, this.props.recipeID, ingredient, index) }/>
-                </td>
-                <td>
-                  <input type="submit" value={`Delete ${ingredient.food_name}`} onClick={e => {this.props.deleteIngredient(e, this.props.recipeID, ingredient); this.props.deleteIngredientLocally(index)} }/>
-                </td>
-              </tr>
+              <>
+                <tr>
+                  <td>{ingredient.food_name}</td>
+                  <td>
+                    <input
+                      type="text"
+                      name="serving_qty"
+                      value={ingredient.serving_qty}
+                      onChange={e=>this.props.handleOnChange(e, index)}
+                    />
+                  </td>
+                  <td>
+                    <input
+                      type="text"
+                      name="serving_unit"
+                      value={ingredient.serving_unit}
+                      onChange={e=>this.props.handleOnChange(e, index)}
+                    />
+                  </td>
+                  <td>{ingredient.calories}</td>
+                  <td>{ingredient.total_carbohydrate}</td>
+                  <td>{ingredient.protein}</td>
+                  <td>{ingredient.sugars}</td>
+                  <td>{ingredient.total_fat}</td>
+                </tr>
+              <input type="submit" value={`Update ${ingredient.food_name}`} onClick={e => this.props.updateIngredient(e, this.props.recipeID, ingredient, index) }/>
+              <input type="submit" value={`Delete ${ingredient.food_name}`} onClick={e => {this.props.deleteIngredient(e, this.props.recipeID, ingredient); this.props.deleteIngredientLocally(index)} }/>
+              </>
             )
           })
         }
