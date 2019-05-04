8f90a450185692253143edf1fcf325a6d18dcdf8
github username <github email address>
2019-05-04 18:32:00 -0400

    begin styling welcome

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Welcome extends Component {
+
+  render() {
+    return (
+      <div className="welcome">
+        <div className="welcome-bg">
+        </div>
+      </div>
+    )
+  }
+
+}
