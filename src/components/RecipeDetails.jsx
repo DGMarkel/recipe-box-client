b05557e5d21accaba3777f659bc88441147fb835
github username <github email address>
2019-05-04 14:34:58 -0400

    display image in recipeDetails

--- src/components/RecipeDetails.jsx
+++ src/components/RecipeDetails.jsx
@@ -13,18 +13,19 @@ export default class RecipeDetails extends Component {
           /><br />
         <textarea
           cols="60"
-          name="image_url"
-          value={this.props.recipe.image_url}
-          placeholder="Image"
-          onChange={event => this.props.handleOnChange(event)}
-        /><br />
-        <textarea
-          cols="60"
           name="description"
           value={this.props.recipe.description}
           placeholder="Brief Description"
           onChange={event => this.props.handleOnChange(event)}
         /><br />
+        <textarea
+          cols="60"
+          name="image_url"
+          value={this.props.recipe.image_url}
+          placeholder="Image"
+          onChange={event => this.props.handleOnChange(event)}
+        /><br />
+        <img src={this.props.recipe.image_url} />
       </div>
     )
   }
