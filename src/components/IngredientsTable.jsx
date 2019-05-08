17b065b62cb696c38cff47ec9b1c256849e9cf62
github username <github email address>
2019-05-08 00:56:30 -0400

    add in missing tag

--- src/components/IngredientsTable.jsx
+++ src/components/IngredientsTable.jsx
@@ -5,23 +5,25 @@ export default class IngredientsTable extends Component {
   render() {
     return (
       <table>
-        <tr>
-          <th>Ingredient</th>
-          <th>Calories(kCal)</th>
-          <th>Carbohydrates(g)</th>
-          <th>Protein(g)</th>
-          <th>Sugars(g)</th>
-        </tr>
-        {this.props.recipe.ingredients.map((ingredient, index) => {
-          return <tr key={index}>
-            <td>{ingredient.food_name}, {ingredient.serving_qty} {ingredient.serving_unit}</td>
-            <td>{Math.floor(ingredient.calories)}</td>
-            <td>{ingredient.total_carbohydrate}</td>
-            <td>{ingredient.protein}</td>
-            <td>{ingredient.sugars}</td>
+        <tbody>
+          <tr>
+            <th>Ingredient</th>
+            <th>Calories(kCal)</th>
+            <th>Carbohydrates(g)</th>
+            <th>Protein(g)</th>
+            <th>Sugars(g)</th>
           </tr>
-        })}
-        </table>
-      )
-    }
+          {this.props.recipe.ingredients.map((ingredient, index) => {
+            return <tr key={index}>
+              <td>{ingredient.food_name}, {ingredient.serving_qty} {ingredient.serving_unit}</td>
+              <td>{Math.floor(ingredient.calories)}</td>
+              <td>{ingredient.total_carbohydrate}</td>
+              <td>{ingredient.protein}</td>
+              <td>{ingredient.sugars}</td>
+            </tr>
+          })}
+        </tbody>
+      </table>
+    )
+  }
 }
