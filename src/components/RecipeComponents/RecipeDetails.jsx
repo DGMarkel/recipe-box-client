353cbe420b9688ab3d12894da0170b457226426e
github username <github email address>
2019-05-08 13:26:41 -0400

    update jsx

--- src/components/RecipeComponents/RecipeDetails.jsx
+++ src/components/RecipeComponents/RecipeDetails.jsx
@@ -3,11 +3,11 @@ import React, { Component } from 'react'
 export default class RecipeDetails extends Component {
   render() {
     return (
-      <div className="full-recipe-card">
-        <h1>{this.recipe.title}</h1>
-        <p>{this.recipe.description}</p>
-        <img src={this.recipe.image_url} alt={this.recipe.title} />
-      </div>
+      <>
+        <h1>{this.props.recipe.title}</h1>
+        <p>{this.props.recipe.description}</p>
+        <img src={this.props.recipe.image_url} alt={this.props.recipe.title} />
+      </>
     )
   }
 }
