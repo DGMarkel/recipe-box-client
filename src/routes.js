89e61cf2d0625870156eaebefa213ece92a2a68f
github username <github email address>
2019-05-07 22:30:22 -0400

    add notes

--- src/routes.js
+++ src/routes.js
@@ -12,7 +12,9 @@ import GenericNotFound from './components/GenericNotFound'
 export default (
     <Switch id='routes'>
       <Route exact path='/' render={ () => <Recipes/> }/>
+      {/*below route is underutilized */} 
       <Route exact path='/dash' component={ () =>  Auth.isUserAuthenticated() ? <Dashboard/> : <Redirect to="/login"/> }/>
+      {/* below route is unused...keeping because I'll prob use it eventually */}
       <Route exact path='/recipes' component={ () =>  <Recipes/> }/>
       <Route exact path='/my-recipes' component={ () =>  Auth.isUserAuthenticated() ? <Recipes user="true"/> : <Redirect to="/login"/> }/>
       <Route exact path='/recipes/new' component={ () =>  Auth.isUserAuthenticated() ? <NewRecipe/> : <Redirect to="/login"/> }/>
