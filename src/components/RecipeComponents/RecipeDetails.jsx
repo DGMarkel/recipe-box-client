4622dd25b56d82a061b270773a28387ec8a9a453
github username <github email address>
2019-05-08 13:02:32 -0400

    update recipeDetails to display info, not a form

--- src/components/RecipeComponents/RecipeDetails.jsx
+++ src/components/RecipeComponents/RecipeDetails.jsx
@@ -3,35 +3,11 @@ import React, { Component } from 'react'
 export default class RecipeDetails extends Component {
   render() {
     return (
-      <>
-        <textarea
-          cols="60"
-          name="title"
-          value={this.props.recipe.title}
-          placeholder="Title"
-          onChange={event => this.props.handleOnChange(event)}
-          /><br />
-        <textarea
-          cols="60"
-          name="image_url"
-          value={this.props.recipe.image_url}
-          placeholder="Image"
-          onChange={event => this.props.handleOnChange(event)}
-        /><br />
-        Servings: <input
-          type="number"
-          name="servings"
-          value={this.props.recipe.servings}
-          onChange={event => this.props.handleOnChange(event)}
-        /><br />
-        <textarea
-          cols="60"
-          name="description"
-          value={this.props.recipe.description}
-          placeholder="Brief Description"
-          onChange={event => this.props.handleOnChange(event)}
-        /><br />
-      </>
+      <div className="full-recipe-card">
+        <h1>{this.recipe.title}</h1>
+        <p>{this.recipe.description}</p>
+        <img src={this.recipe.image_url} alt={this.recipe.title} />
+      </div>
     )
   }
 }
