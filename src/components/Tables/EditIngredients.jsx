fc1367d2b66d6bfebe57f50c82ad19d882c4d787
github username <github email address>
2019-05-14 22:44:24 -0400

    update buttons with callbacks

--- src/components/Tables/EditIngredients.jsx
+++ src/components/Tables/EditIngredients.jsx
@@ -16,31 +16,35 @@ export default class IngredientsTable extends Component {
             <th>Sugars(g)</th>
           </tr>
           {this.props.ingredients.map((ingredient, index) => {
-            return <tr key={index}>
-              <td>{ingredient.food_name}</td>
-              <td>
-                <input
-                  type="text"
-                  name="serving_qty"
-                  value={ingredient.serving_qty}
-                  onChange={e=>this.props.handleOnChangeForIngredients(e, index, this.props.recipeID)}
-                />
-              </td>
-              <td>
-                <input
-                  type="text"
-                  name="serving_unit"
-                  value={ingredient.serving_unit}
-                  onChange={e=>this.props.handleOnChangeForIngredients(e, index, this.props.recipeID)}
-                />
-              </td>
-              <td>{Math.floor(ingredient.calories)}</td>
-              <td>{ingredient.total_carbohydrate}</td>
-              <td>{ingredient.protein}</td>
-              <td>{ingredient.sugars}</td>
-              <td><button>Update</button></td>
-              <td><button>Delete</button></td>
-            </tr>
+            return (
+              <form>
+                <tr key={index}>
+                  <td>{ingredient.food_name}</td>
+                  <td>
+                    <input
+                      type="text"
+                      name="serving_qty"
+                      value={ingredient.serving_qty}
+                      onChange={e=>this.props.handleOnChangeForIngredients(e, index, this.props.recipeID)}
+                    />
+                  </td>
+                  <td>
+                    <input
+                      type="text"
+                      name="serving_unit"
+                      value={ingredient.serving_unit}
+                      onChange={e=>this.props.handleOnChangeForIngredients(e, index, this.props.recipeID)}
+                    />
+                  </td>
+                  <td>{Math.floor(ingredient.calories)}</td>
+                  <td>{ingredient.total_carbohydrate}</td>
+                  <td>{ingredient.protein}</td>
+                  <td>{ingredient.sugars}</td>
+                  <td><button onClick={e => this.props.updateIngredient(e, this.props.recipeID, ingredient.food_name, this.props.ingredients[index], index) }>Update</button></td>
+                  <td><button onClick={e => this.props.deleteIngredient(e, this.props.recipeID, ingredient) }>Delete</button></td>
+                </tr>
+              </form>
+            )
           })}
         </tbody>
       </table>
