fd8b7df67878340583fecbf1c0746497ec978ac9
github username <github email address>
2019-05-14 20:07:53 -0400

    update css, move jsx elements

--- src/components/RecipeComponents/EditRecipeDetails.jsx
+++ src/components/RecipeComponents/EditRecipeDetails.jsx
@@ -3,6 +3,9 @@ import React, { Component } from 'react'
 export default class EditRecipeDetails extends Component {
   render() {
     return (
+      <>
+      <h1>{this.props.recipe.title}</h1>
+      <span className="fake-link" onClick={()=>this.props.editToggler()}>Close Edit Form</span>
       <form onSubmit={e=>this.props.updateRecipeDetails(e, this.props.recipe)}>
         <textarea
           cols="60"
@@ -32,8 +35,8 @@ export default class EditRecipeDetails extends Component {
           onChange={event => this.props.handleOnChangeForRecipeDetails(event, this.props.recipe.id)}
         /><br />
         <input type="submit" value="Update Recipe Details" /><br />
-        <span className="fake-link" onClick={()=>this.props.editToggler()}>Close</span>
       </form>
+      </>
     )
   }
 }
