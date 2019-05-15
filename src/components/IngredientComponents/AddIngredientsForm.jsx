e12bc2d13513e8e4e2630126621a8349923d1ce6
github username <github email address>
2019-05-14 23:13:12 -0400

    add state and onChange method to addIngredientsForm

--- src/components/IngredientComponents/AddIngredientsForm.jsx
+++ src/components/IngredientComponents/AddIngredientsForm.jsx
@@ -1,16 +1,29 @@
 import React, { Component } from 'react'
 
 export default class AddIngredientsForm extends Component {
+  constructor() {
+    super()
+    this.state={
+      rawIngredients: ''
+    }
+  }
+
+  handleOnChange = e => {
+    this.setState({
+      rawIngredients: e.target.value
+    })
+  }
+
   render() {
     return (
-       <form onSubmit={e => this.props.fetchAndPostIngredients(e, this.props.rawIngredients, this.props.recipe) }>
+       <form onSubmit={e => this.props.fetchAndPostIngredients(e, this.state.rawIngredients, this.props.recipe) }>
         <textarea
           rows="10"
           cols="60"
           name="rawIngredients"
           value={this.props.rawIngredients}
-          placeholder="Ingredient"
-          onChange={e => this.props.handleOnChange(e)}
+          placeholder="Add Ingredients"
+          onChange={e => this.handleOnChange(e)}
           /><br />
         <input type="submit" value="Save Ingredients" />
       </form>
