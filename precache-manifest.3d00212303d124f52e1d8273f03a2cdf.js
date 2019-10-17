6601826499b768c37860963fc0c1d948b12cf16a
github username <github email address>
2019-10-16 22:42:25 -0400

    Updates

---
+++ precache-manifest.3d00212303d124f52e1d8273f03a2cdf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4bcbcdd69a32b77b67d",
+    "url": "/recipe-box-client/static/css/main.0775805d.chunk.css"
+  },
+  {
+    "revision": "a4bcbcdd69a32b77b67d",
+    "url": "/recipe-box-client/static/js/main.6409adfb.chunk.js"
+  },
+  {
+    "revision": "4ee946311d84e3f9c3eb",
+    "url": "/recipe-box-client/static/js/runtime~main.bc8e9303.js"
+  },
+  {
+    "revision": "5f48d5a18d359c7601e7",
+    "url": "/recipe-box-client/static/js/2.dd711d12.chunk.js"
+  },
+  {
+    "revision": "ce02f0a5f4d92fcec8e59e8f05f9ad8c",
+    "url": "/recipe-box-client/index.html"
+  }
+];
