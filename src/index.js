6f40047babf009bb9ef115359c9a357a66c8a779
David Markel <davidgmarkel@gmail.com>
2019-10-29 15:14:51 -0400

    comment out redux devtools extensions to allow browser compatibility (otherwise site only runs on browsers with redux devtools installed)

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ import App from './App';
 
 const store = createStore(PrelimReducer,
   compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(thunk)
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() ONLY TO BE USED IN DEVELOPMENT
   )
 )
 
